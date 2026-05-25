bfaac7c78502935ec1b286c87e232060afdb3ac9
MajorMurphy <major@murphyindustries.net>
2026-05-25 11:15:29 -0400

    [libultrahdr] updated

--- ports/libultrahdr/portfile.cmake
+++ ports/libultrahdr/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/libultrahdr
-    REF ea518da3a839c59f88ffc6c4020920c944251fd2
-    SHA512 d2a2058845255a336645bb23fa1da55f44a5bb1d8e374e034319b7a4bd2ac9d44369144978ca85b70915a40131f98c094c798283208e0d7e09dbb8cc0a20637e
+    REF 82b4f6da9f4db25f51a3a40201ab6eb86b6d6bb7
+    SHA512 52bc848c3c936548ff457e31bf7c8ae7fcf88edb32ea06650fce1b4374c03f3bdf0eef013e985dde8cb4832a7db86d39fcd7ee025605fdd4619cbd5f7b76c51e
     HEAD_REF master
 )
 vcpkg_cmake_configure(
